69fafe6ba9242f092b91234c49a58c7bea593a53
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-08-20 14:12:58 +0900

    create ChangeToMyRichText

--- server/models/my_rich_text.go
+++ server/models/my_rich_text.go
@@ -1,3 +1,18 @@
+
 package models
 
+import "server/api/notion"
 
+
+type MyRichText struct {
+	Model
+	RichTextBlockID string
+	notion.RichText
+}
+
+func ChangeToMyRichText(richText *notion.RichText,blockID string)(*MyRichText){
+	newMyRichText := new(MyRichText)
+	newMyRichText.RichText = *richText
+	newMyRichText.RichTextBlockID = blockID
+	return newMyRichText
+}
