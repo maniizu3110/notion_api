b1e4d4d8062dd068adb216c81cdc85dfccaa3166
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-09-03 13:36:32 +0900

    コメント

--- server/middlewares/config.go
+++ server/middlewares/config.go
@@ -13,6 +13,7 @@ import (
 func SetConf(config util.Config, db *gorm.DB) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			//TODO:txいらなそう？？migrate部分と合わせて確認する
 			tx := db.Begin()
 			//TODO：API叩かれたときではなく、サーバー起動時に呼ばれるべき処理?
 			database.Migrate(tx)
